7598588505d108ebfbc4c89959325cdce6f6ba8d
Alexander Berezin <alex@lmcx.ru>
2022-06-04 00:52:28 +0300

    Added normalize.css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import 'normalize.css';
 
 createApp(App)
     .use(ElementPlus)
