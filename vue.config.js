121258a41c0d9f7e3d604b96733b8e63776456d8
Alexander Berezin <alex@lmcx.ru>
2022-06-21 23:43:20 +0300

    removed unplugin-auto-import

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service');
-const AutoImport = require('unplugin-auto-import/webpack');
 const Components = require('unplugin-vue-components/webpack');
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
 
@@ -8,10 +7,6 @@ module.exports = defineConfig({
 
     configureWebpack: {
         plugins: [
-            AutoImport({
-                resolvers: [ElementPlusResolver()],
-            }),
-
             Components({
                 resolvers: [ElementPlusResolver()],
             }),
